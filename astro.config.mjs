c1d064dba3b689d6ea946438d7fda0f809303a42
vcc0827 <vcc0827@github.com>
2024-09-07 11:14:56 +0800

    update: defineConfig settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vcc0827.github.io',
-  base: 'my-repo',
   integrations: [mdx(), sitemap(), tailwind()]
 });
